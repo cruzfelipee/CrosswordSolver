f9f2bbb89cd95092901b8474e1807b3ab05c1da4
Felipe Valiati <felipe.cvaliati@gmail.com>
2024-10-09 14:17:52 -0300

    chore: deletei invertmatrix e replacecharacter

--- Utilities.py
+++ Utilities.py
@@ -2,16 +2,6 @@ def printMatrix(m):
     for i in m:
         print(i.strip())
 
-def replaceCharacter(s : str, index : int, character : str):
-    pass #da replace no cahracter
-
-def invertMatrix(m):
-    for x in range(len(m)):
-        for y in range(len(m[x])):
-            aux = m[x][y]
-            m[x][y] = m[y][x] # troca essas 2 linhas pelo replacecharacter
-            m[y][x] = aux
-
 def getMaxLenWord(word_list):
     maxLen = 0
     for i in word_list:
